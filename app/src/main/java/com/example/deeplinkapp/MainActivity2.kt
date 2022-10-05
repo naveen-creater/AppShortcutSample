41154dd69c0fd0b2bf774c446e021b0967ae5986
Naveenkumar.k <naveenkumar.k@marketsimplified.com>
2022-10-05 22:49:00 +0530

    static and dynamic

--- app/src/main/java/com/example/deeplinkapp/MainActivity2.kt
+++ app/src/main/java/com/example/deeplinkapp/MainActivity2.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        setContentView(R.layout.activity_main2)
     }
 }
--- app/src/main/java/com/example/deeplinkapp/MainActivity3.kt
+++ app/src/main/java/com/example/deeplinkapp/MainActivity3.kt
@@ -1,11 +1,36 @@
 package com.example.deeplinkapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main5)
+        setContentView(R.layout.activity_main3)
+
+        findViewById<Button>(R.id.button2).setOnClickListener(View.OnClickListener {
+            dynamicShortcut()
+        })
+
+    }
+
+    fun dynamicShortcut(){
+        val shortcut = ShortcutInfoCompat.Builder(this, "dynamic")
+            .setShortLabel("Website")
+            .setLongLabel("Open the website")
+            .setIcon(IconCompat.createWithResource(this, R.drawable.ic_launcher_foreground))
+            .setIntent(
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
+            )
+            .build()
+
+        ShortcutManagerCompat.pushDynamicShortcut(this, shortcut)
     }
 }
